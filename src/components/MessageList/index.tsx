05a99d42e0bd6d3f4917dc3dbcb13b278c1605d0
Vinicius <vini.bracht1@gmail.com>
2021-10-21 22:28:33 -0300

    user auth add

---
+++ src/components/MessageList/index.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import styles from './styles.module.scss';
+import logoImg from '../../assets/logo.svg';
+import { api } from '../../services/api';
+
+type Message = {
+  id: string;
+  text: string;
+  user: {
+    name: string;
+    avatar_url: string;
+  };
+};
+
+export function MessageList() {
+  const [messages, setMessages] = React.useState<Message[]>([]);
+
+  React.useEffect(() => {
+    //chamada para a api
+    api.get<Message[]>('messages/last3').then((response) => {
+      setMessages(response.data);
+    });
+  }, []);
+
+  return (
+    <div className={styles.messageListWrapper}>
+      <img src={logoImg} alt="DoWhile 2021" />
+
+      <ul className={styles.messageList}>
+        {messages.map((message) => {
+          return (
+            <li key={message.id} className={styles.message}>
+              <p className={styles.messageContent}>{message.text}</p>
+              <div className={styles.messageUser}>
+                <div className={styles.userImage}>
+                  <img src={message.user.avatar_url} alt={message.user.name} />
+                </div>
+                <span>{message.user.name}</span>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
